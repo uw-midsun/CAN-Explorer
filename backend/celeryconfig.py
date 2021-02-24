c27ed47b553c8ba47ae32fd3cc97cebcd52d4011
vagrant <vagrant@midsunbox>
2021-02-24 03:02:11 +0000

    upgraded celery and related files

---
+++ backend/celeryconfig.py
@@ -0,0 +1,8 @@
+broker_url = 'redis://'
+result_backend = 'redis://'
+
+task_serializer = 'json'
+result_serializer = 'json'
+accept_content = ['json']
+timezone = 'Europe/Oslo'
+enable_utc = True
--- backend/django_app/celery.py
+++ backend/django_app/celery.py
@@ -5,14 +5,20 @@ from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
-app = Celery('django_app')
+app = Celery('django_app', backend='redis://', broker='redis://')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
+app.conf.broker_url = 'redis://localhost:6379/0'
+app.conf.result_backend = 'redis://localhost:6379/0'
+
+app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+if __name__ == '__main__':
+    app.start()
