799a636b19fd0bb6b6d2c3c6f6260b65a80eacab
Raiyan Sayeed <raiyan.business@gmail.com>
2021-03-13 19:34:10 +0000

    backend is running now

--- api/django_app/settings.py
+++ api/django_app/settings.py
@@ -24,7 +24,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
-    'can_server',
     'can_server.apps.CanServerConfig',
     'rest_framework',
     'corsheaders',
