1ffbbb0dc9eba963dc2098443de9a4f79675a25b
Raiyan Sayeed <raiyan.business@gmail.com>
2021-06-02 21:17:37 -0400

    changed entrypoint so new graph doesn't get shown upon startup each time

--- influxdb/docker-entrypoint.sh
+++ influxdb/docker-entrypoint.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# https://forums.docker.com/t/best-practice-to-run-a-bash-setup-script-after-creation-of-container/28988/2
-# create setup here
-
 influxd &
 
-sleep 20
+# check if config has already been setup 
+if [[ $(sed -n '/^\[orig-config\]/p;q' /etc/influxdb2/influx-configs) != "[orig-config]" ]]
+then
+    sleep 20
+    influx setup --name orig-config --org midnightsun --bucket orig_bucket --username firmware --password ilovecans --token 123456 --force
+    influx bucket create -n raw_data -o midnightsun -t 123456
+    influx bucket create -n converted_data -o midnightsun -t 123456
+    influx apply -o midnightsun -f /var/lib/influxdb2/can_explorer_template.yml --force true
+    echo "Setup done!"
+fi
 
-influx setup --name orig-config --org midnightsun --bucket orig_bucket --username firmware --password ilovecans --token 123456 --force
-influx bucket create -n raw_data -o midnightsun -t 123456
-influx bucket create -n converted_data -o midnightsun -t 123456
-influx apply -o midnightsun -f /var/lib/influxdb2/can_explorer_template.yml --force true
-
-echo "Setup done!"
+echo "Starting up"
 
 sleep infinity
